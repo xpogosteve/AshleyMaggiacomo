fedad1ac81c175b9cdb51aee0c5cd600b1f5f3b3
Steven <xpogosteven@gmail.com>
2019-02-10 21:09:55 -0500

    largely completed

--- js/projects.js
+++ js/projects.js
@@ -1,19 +1,19 @@
 $(document).ready(function (){
-  $("div").css("margin-top", ($("ul").outerHeight()) + 15 + "px");
+  $("div").css("margin-top", ($("ul").outerHeight()) + 10 + "px");
   var map = new Map([["nxt",$(".next")],["ov",$(".ov")],["bl",$(".bl")],
   ["wo",$(".wo")],["lb",$(".lb")]]);
   $("li a").click(function () {
     var element = map.get($(this).attr("id"));
     if(!(element.hasClass("shown"))){
-      $(".shown").hide(90);
+      $(".shown").hide();
       $(".shown").removeClass("shown");
       $(".active").removeClass("active");
       $(this).addClass("active");
       element.addClass("shown");
-      element.show(100);
+      element.show();
     }
   });
 });
 $(window).on('resize', function (){
-  $("div").css("margin-top", ($("ul").outerHeight()) + 15 + "px");
+  $("div").css("margin-top", ($("ul").outerHeight()) + 10 + "px");
 });
